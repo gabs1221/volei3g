4ffe2605ffd00ea7689be83b3f41a64359f578e5
a <108934829+Luizfernandouu@users.noreply.github.com>
2024-08-28 08:39:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+
+let entrevista_1 = {
+    entrevistador: "Gabriel",
+    entrevistado: "Arthur",
+    questoes: {
+        p1: "Qual era seu sonho profissional quando pequeno?",
+        r1: "Quando era pequeno, sempre sonhei em ser jogador de futebol. Eu passava horas jogando na rua.",
+        p2: "Como você imagina seu estilo de vida ideal no futuro?",
+        r2: "Meu estilo de vida ideal seria tranquilo e equilibrado. Gostaria de viver em um ambiente calmo e ter tempo para relaxar.",
+        p3: "Qual é a importância das relações pessoais em sua vida futura?",
+        r3: "As relações pessoais serão extremamente importantes. Valorizo muito o tempo com amigos e familiares.",
+        p4: "Onde você se vê profissionalmente daqui a cinco anos?",
+        r4: "Daqui a cinco anos, me vejo trabalhando em um clube de esportes ou gerenciando projetos em empresas.",
+        p5: "O que é mais importante para você: uma carreira bem-sucedida, equilíbrio entre vida pessoal e profissional, ou outra coisa?",
+        r5: "Todos esses fatores são importantes. Desejo uma carreira de sucesso, mas também encontrar um equilíbrio saudável.",
+        p6: "Como você planeja equilibrar sua vida financeira no futuro? Tem algum plano para economizar, investir ou gastar de forma consciente?",
+        r6: "Sim, tenho um plano para ser disciplinado com meus gastos, investir com sabedoria e economizar para o futuro.",
+        p7: "Como você imagina sua saúde e bem-estar ao longo dos anos?",
+        r7: "Imagino minha saúde sendo muito boa, pois sempre pratiquei atividades físicas e cuido da minha alimentação.",
+        p8: "Há algum lugar que planeja morar no futuro?",
+        r8: "Sim, sonho em morar na Suíça. Sempre admirei a qualidade de vida e a beleza natural do país."
+    }
+}
+
+let entrevista_2 = {
+    entrevistador: "Luiz",
+    entrevistado: "Matheus",
+    questoes: {
+        p1: "Qual era seu sonho profissional quando pequeno?",
+        r1: "Meu sonho sempre foi ser jogador de volei. Eu passava muito tempo treinando e sonhando em jogar em grandes estádios.",
+        p2: "Como você imagina seu estilo de vida ideal no futuro?",
+        r2: "Espero ter uma vida tranquila e equilibrada. Quero viver em um lugar sereno e ter tempo para aproveitar as pequenas coisas.",
+        p3: "Qual é a importância das relações pessoais em sua vida futura?",
+        r3: "Relações pessoais são essenciais. Acredito que estar próximo de amigos e família é fundamental para a felicidade.",
+        p4: "Onde você se vê profissionalmente daqui a cinco anos?",
+        r4: "Imagino-me trabalhando em um clube esportivo ou liderando projetos em uma empresa. Quero estar em um ambiente desafiador.",
+        p5: "O que é mais importante para você: uma carreira bem-sucedida, equilíbrio entre vida pessoal e profissional, ou outra coisa?",
+        r5: "Todos esses aspectos são importantes. Desejo ter sucesso na carreira, mas também equilibrar bem vida pessoal e profissional.",
+        p6: "Como você planeja equilibrar sua vida financeira no futuro? Tem algum plano para economizar, investir ou gastar de forma consciente?",
+        r6: "Sim, planejo ser cauteloso com meus gastos, investir de forma inteligente e manter uma reserva financeira para imprevistos.",
+        p7: "Como você imagina sua saúde e bem-estar ao longo dos anos?",
+        r7: "Imagino minha saúde sendo excelente, pois sempre pratiquei esportes e cuido da minha alimentação e bem-estar.",
+        p8: "Há algum lugar que planeja morar no futuro?",
+        r8: "Gostaria de morar na Suíça. A qualidade de vida e o ambiente tranquilo são muito atraentes para mim."
+    }
+}
+
+let entrevista_3 = {
+    entrevistador: "Arthur",
+    entrevistado: "Gabriel",
+    questoes: {
+        p1: "Qual era seu sonho profissional quando pequeno?",
+        r1: "Quando era criança, sonhava em ser jogador de futebol. O esporte sempre me fascinou e eu treinava sempre que podia.",
+        p2: "Como você imagina seu estilo de vida ideal no futuro?",
+        r2: "Visualizo um estilo de vida calmo e equilibrado. Quero viver de forma tranquila, com tempo para relaxar e aproveitar a vida.",
+        p3: "Qual é a importância das relações pessoais em sua vida futura?",
+        r3: "As relações pessoais são muito importantes. Manter laços fortes com amigos e familiares é essencial para a felicidade.",
+        p4: "Onde você se vê profissionalmente daqui a cinco anos?",
+        r4: "Me vejo em um papel de destaque em um clube esportivo ou gerenciando projetos em uma empresa, buscando crescimento contínuo.",
+        p5: "O que é mais importante para você: uma carreira bem-sucedida, equilíbrio entre vida pessoal e profissional, ou outra coisa?",
+        r5: "Todos esses fatores são importantes. Desejo sucesso na carreira, mas também manter um bom equilíbrio entre trabalho e vida pessoal.",
+        p6: "Como você planeja equilibrar sua vida financeira no futuro? Tem algum plano para economizar, investir ou gastar de forma consciente?",
+        r6: "Sim, tenho um plano. Pretendo ser prudente com meus gastos, investir de maneira inteligente e economizar para garantir segurança financeira.",
+        p7: "Como você imagina sua saúde e bem-estar ao longo dos anos?",
+        r7: "Espero manter uma saúde excelente. Sempre fiz exercícios e me preocupo com uma alimentação saudável, o que ajuda a manter o bem-estar.",
+        p8: "Há algum lugar que planeja morar no futuro?",
+        r8: "Sim, planejo morar na Suíça. A qualidade de vida e a beleza natural do país são muito atraentes para mim."
+    }
+}
+
+let entrevista_4 = {
+    entrevistador: "Matheus",
+    entrevistado: "Luiz",
+    questoes: {
+        p1: "Qual era seu sonho profissional quando pequeno?",
+        r1: "Quando era pequeno, meu sonho era ser jogador de basquete. O esporte sempre foi uma grande paixão para mim.",
+        p2: "Como você imagina seu estilo de vida ideal no futuro?",
+        r2: "Imagino uma vida tranquila e equilibrada. Quero viver em um ambiente calmo e ter tempo para aproveitar cada momento.",
+        p3: "Qual é a importância das relações pessoais em sua vida futura?",
+        r3: "As relações pessoais são fundamentais. Ter uma rede de apoio e manter laços com pessoas queridas é crucial para uma vida plena.",
+        p4: "Onde você se vê profissionalmente daqui a cinco anos?",
+        r4: "Daqui a cinco anos, me vejo atuando em um clube esportivo ou gerenciando projetos em uma empresa. Quero estar em um papel que me desafie.",
+        p5: "O que é mais importante para você: uma carreira bem-sucedida, equilíbrio entre vida pessoal e profissional, ou outra coisa?",
+        r5: "Para mim, todos esses aspectos são importantes. Busco sucesso na carreira, mas também um bom equilíbrio entre vida pessoal e profissional.",
+        p6: "Como você planeja equilibrar sua vida financeira no futuro? Tem algum plano para economizar, investir ou gastar de forma consciente?",
+        r6: "Sim, planejo ser estratégico com minhas finanças. Vou economizar, investir de forma consciente e controlar meus gastos.",
+        p7: "Como você imagina sua saúde e bem-estar ao longo dos anos?",
+        r7: "Espero manter uma boa saúde, pois sempre pratiquei atividades físicas e me preocupo com uma alimentação saudável.",
+        p8: "Há algum lugar que planeja morar no futuro?",
+        r8: "Sim, gostaria de morar na Suíça. A qualidade de vida e a tranquilidade do país são muito atraentes."
+    }
+}
